f4f7d6d014466d7ea4845876d1a83c1cd423aa06
荒井千里 <araisenri@araisenrinoMacBook-Pro.local>
2021-10-13 18:22:56 +0900

    線形ループの追加

--- src/sampleMain.java
+++ src/sampleMain.java
@@ -3,6 +3,12 @@ public class sampleMain {
     public static void main(String... args){
 
         System.out.println("hello intellij");
-
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i < 10; i++){
+            System.out.println(a * i + b);
+            a += 3;
+            b += 5;
+        }
     }
 }
